9d82a5e4a05daa0be12343f5add90a87f83c63d9
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2024-02-08 11:26:08 +0400

    Initial commit

---
+++ RBTest/RBTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RBTestApp.swift
+//  RBTest
+//
+//  Created by Anton on 08.02.24.
+//
+
+import SwiftUI
+
+@main
+struct RBTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
